0b991e52fd1d387fd0263b85e8e2e127d1b168f1
ankit257184 <ankit_257184@saitm.ac.in>
2025-11-13 23:12:05 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,1210 @@
+# print("hello world")
+# if 5>2:
+#     print("jvjvjdh)
+
+# PRINT("hello")
+# print("hello")
+# print("my name is abc")
+# x=5
+# x=5
+# print(x+y)
+# x=5
+# y=3
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# #jknjkhiugytfyt
+# hjghgfghf
+# #x=5
+
+
+# print("this is python")
+# print('hello')
+# x=5
+# y="hello"
+# print(type(x))
+# print(type(y))
+
+
+
+
+# x=4.5
+# print(type(x))
+# x=true
+
+# x=5
+# y=4
+# print(bool(x+y))
+# print(bool(x))
+
+# x=""
+# print(bool(x))
+# x="e"
+# print(bool(x))
+
+# my_var=40
+# print(my_var)
+
+# my-var=40
+# my var=40
+
+# x,y,z="30","40","50"
+# print(x)
+# x,y,z="apple"
+# print(x)
+
+# fruits=["apple","banana","cherry"]
+# x,y,z="apple"
+# x=y=z="apple"
+# print(x)
+
+# fruits=["apple","banana","cherry"]
+# x,y,z=fruits
+# print(y)
+# x=y=z=fruits
+# print(x)
+# fruits=[2,4,7,8,34,50]
+# print(fruits)
+# print(type(fruits))
+
+
+
+# x="this is python"
+# print(x)
+
+# x="python"
+# y="is"
+# z="good"
+# print(x,y,z)
+# print(x+y+z)
+
+# def my_function():
+#     print("python is " +x)
+# my_function()
+
+# x="good"
+
+# def my_function():
+#     global x
+#     x="good"
+# my_function()
+
+# print("python is" + x)
+
+# data types
+
+# x=5
+# print(type(X))
+
+# x=5
+# print(type(x))
+
+# x=5.6
+# print(type(x))
+
+# x="5.6"
+# print(type(x))
+
+# x=1j
+# print
+
+# x=1j
+# print(type(x))
+
+# x={"apple","banana"}
+# print(type(x))
+
+# x="good"
+# print(range(x))
+
+# x="good
+
+# x="good"
+# print(range)
+
+
+# my_list=["apple","banana","cherry"]
+# my_list.append("guvava")
+# print(my_list)
+
+
+# my_list=["apple","banana",1,3,5,7]
+
+# my_list[2]="abc"
+
+# print(my_list)
+
+# my_list=["apple","banana",1,3,5,7]
+
+# my_list[2:5]="abc"
+
+# print(my_list)
+
+# my_list=["apple","banana",1,3,5,7]
+
+# my_list[2:5]=["abc"]
+
+# print(my_list)
+
+# my_list=[1,2,4,5,6]
+
+# my_list.insert(2,3)
+
+# print(my_list)
+
+# my_list=[1,2,4,5,6]
+
+# my_list.append("hghgfd")
+
+# print(my_list)
+
+
+
+# #extend
+# list1=[1,2,3]
+# list2=[4,5,6]
+
+# list1.extend(list2)
+
+# print(list1)
+
+# #append
+# list1=[1,2,3]
+# list2=[4,5,6]
+
+# list1.append(list2)
+
+# print(list1)
+
+# #remove
+# list1=[1,2,3,"apple",4,5,6]
+
+# list1.remove("apple")
+# print(list1)
+
+# #pop
+# list1=[1,2,3,"apple",4,5,6]
+
+# list1.pop()
+# print(list1)
+
+# #task
+
+# list1=["1","2","3","4","5","6"]
+# list1.append("apple")
+# print(list1)
+
+# list1.insert(-1,"banana")
+# print(list1)
+
+# list1.pop()
+# print(list1)
+
+# list1.remove("banana")
+# print(list1)
+
+# list1.clear()
+# print(list1)
+
+# list1=[]
+# list1.append(1)
+# print(list1)
+
+# list1.append('hello')
+# print (list1)
+
+# #loopslists
+
+# list1=[1,2,3,4,5,6]
+# for x in list1:
+#     print(x)
+
+ 
+# #sort
+# list=[2,5,14,8]
+
+# list.sort()
+# print(list)
+
+# list=[2,5,14,8]
+
+# list.sort(reverse=True)
+# print(list)
+
+
+# text="hello Python"
+# print(text.upper())
+# print(text.lower())
+# print(text.strip())
+# print(text.replace("Python","world"))
+# print(text.split())
+
+# x=5
+# y="ghgfhg"
+
+# #format string
+
+# Name="Abc"
+# age=18
+# print(x+y)
+
+# x="Abc"
+# y=18
+# print(f"my name is {x} and my age is {y}")
+
+# x=5
+# x+=3
+
+# print(x)
+
+# y=10
+# y*=2
+# print(y)
+
+# x=10
+# x-=4
+# print(x)
+
+# y=40
+# y/=2
+# print(y)
+
+# x=5
+# y=10
+
+# print(x>y)
+# print(x<y)
+# print(x==y)
+# print(x!=y)
+# print(x>=y)
+# print(x<=y)
+
+
+# x=5
+# print(x>3 and x<10)
+# print(not(x>3))
+
+# x=8
+# y=x
+# z=10
+# print(y)
+# print(z is y)
+# print(x is y)
+# print(x is not y)
+
+# colors={"red","green","blue"}
+# print(colors)
+
+
+# colors.add("yellow")
+# print(colors)
+
+# colors.remove("green")
+# print(colors)
+
+# for color in colors:
+#     print(color)
+
+# students={
+#     "name":"chander",
+#     "age":30,
+#     "grades":"A"
+# }
+# print(students)
+
+# for key,value in students.items():
+#     print(key,value)
+
+
+#if else
+
+# age=20
+
+# if age>=18:
+#     print("you are adult")
+# else:
+#     print("you are not adult")
+
+# marks=85
+
+# if marks>=90:
+#     print("grade A")
+# elif marks>=75:
+#     print("grade A")
+# elif marks>=60:
+#     print("grade B")
+# else:
+#     print("grade C")
+
+
+
+
+# age=16
+# if age>18:
+#     print("you are adult")
+# elif age==18:
+#     print("you are teen")
+# else:
+#     print("you are not adult")
+
+
+# age=16
+# if age>18:
+#     print("you are adult")
+# elif age==18:
+#     print("you are teen")
+# elif age==17:
+#     print("you are 17")
+# elif age==16:
+#     print("you are 16")
+# else:
+#     print("you are not adult")
+
+
+
+# x=15
+
+# if x>10:
+#     print("x is greater than 10")
+#     if x>20:
+#         print("x is also greater than 20")
+#     else:
+#         print("x is not greater than 20")
+
+
+
+# x=4
+# if x%2==0:
+#     print("even")
+# else:
+#     print("odd")
+
+# age=14
+# if age>=18:
+#     print("not eligible for driving")
+# else:
+#     print("eligible for driving")
+
+
+
+# fruits=["apple","banana","cherry"]
+
+# for fruit in fruits:
+#     print(fruit)
+
+
+
+# word="python"
+
+# for x in word:
+#     print(x)
+
+
+# for i in range(5):
+#     print(i)
+
+
+# for i in range(1,10,2):
+#     print(i)
+
+
+
+# for i in range(1,4):
+#     for j in range(1,3):
+#         print(f"i={i},j={j}")
+
+
+# for i in range(1,6):
+#     if i==4:
+#         break
+#     print(i)
+
+
+# for i in range(1,6):
+#     if i==3:
+#         continue
+#     print(i)
+
+
+# for i in range (1,21):
+#     print(i)
+
+
+# for i in range(1,30):
+#     if i%2!=0:
+#         continue
+#     print(i)
+
+# for i in range(1,10):
+#     if i==7:
+#         break
+#     print(i)
+
+
+#functions
+
+# def greet():
+#     print("hello python")
+
+# greet()
+
+
+# def greet(name):
+#     print(f"hello,{name}")
+
+# greet("alice")
+# greet("abc")
+
+# def add(a,b):
+#     return a+b
+
+# result =add(5,3)
+# print(result)
+
+# def sub(a,b):
+#     return a-b
+
+# result =sub(5,3)
+# print(result)
+
+# def product(a,b):
+#     return a*b
+
+# result =product(5,3)
+# print(result)
+
+# def divide(a,b):
+#     return a/b
+
+# result =divide(5,3)
+# print(result)
+
+# def greet(name="student"):
+#     print(f"hello,{name}")
+
+# greet()
+# greet("alice")
+
+# x=12
+ 
+# def my_func():
+
+#     y=5
+#     print(x,y)
+# my_func()
+
+# print(x)
+
+
+# def greet():
+#     print("Hello!")
+
+# greet()
+
+#def add(a,b):
+#     return a+b
+
+# result =add(5,3)
+# print(result)
+
+# def greet(name="abc"):
+#      print(f"hello,{name}")
+
+# greet()
+
+
+#class
+# class Car:
+#     def __init__(self, brand, color):
+#         self.brand=brand
+#         self.color=color
+
+#     def drive(self):
+#         print(f"{self.color} {self.brand} is driving")
+
+# #object
+# car1=Car("BMW","Black")
+# car2=Car("Tesla","White")
+
+# car1.drive()
+# car2.drive()
+
+#array
+
+# import array
+
+# number=array.array('i',[1,2,3,4,5])
+# print(number)
+
+
+
+# import array
+
+# number=array.array[1,2,3,4,5]
+# print(number)
+
+
+
+# from numpy import random
+
+# x=random.randint(100)
+# print(x)
+
+# x=random.rand()
+# print(x)
+# print(type(x))
+
+
+# [54]
+# [1,2,3,4,5]
+
+# [[1,2,3],
+# [4,5,6]]
+
+# from numpy  import random
+
+# x=random.randint(100,size=(5))
+# print(x)
+
+# x=random.randint(100,size=(3,5))
+# print(x)
+
+# x=random.randint(100,size=(2,3,5))
+# print(x)
+
+# x=random.randint(100,size=(2,2,3,5))
+# print(x)
+
+# x=random.choice([4,5,6])
+# print(x)
+
+# x=random.choice([4,5,6] , size=(5))
+# print(x)
+
+# x=random.choice([4,5,6] , size=(2,3,5))
+# print(x)
+
+
+# pandas
+
+
+
+
+# import pandas as pd
+
+# data=[10,20,30,40]
+# s=pd.Series(data)
+
+# print(s)
+
+# data={
+#     "Name":["Alice","Bob","Charlie","David"],
+#     "Age":[24,27,22,32],
+#     "City":["Delhi","Mumbai","Chennai","Kolkata"]
+
+# }
+
+# df=pd.DataFrame(data)
+
+# print(df)
+
+# import numpy as np
+
+# arr=np.array([1,2,3,4,5])
+# s=pd.Series(arr)
+
+# print(s)
+
+
+# data={"Math":90,"Science":85,"English":78}
+# s=pd.Series(data)
+# print(s)
+
+import pandas as pd
+df = pd.read_csv("crocodile_dataset.csv")
+print(df.head())
+print(df.tail())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
